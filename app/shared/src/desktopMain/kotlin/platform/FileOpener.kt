6d065fb69c3bdfe4b8e1467b6e9b191ff446e019
Him188 <Him188@mamoe.net>
2024-10-28 00:27:41 +0000

    Migrate FileOpener ProcessBuilder usage

--- app/shared/src/desktopMain/kotlin/platform/FileOpener.kt
+++ app/shared/src/desktopMain/kotlin/platform/FileOpener.kt
@@ -1,19 +1,10 @@
 /*
- * Animation Garden App
- * Copyright (C) 2022  Him188
+ * Copyright (C) 2024 OpenAni and contributors.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * https://github.com/open-ani/ani/blob/main/LICENSE
  */
 
 package me.him188.ani.app.platform
@@ -52,15 +43,15 @@ object FileOpener {
             when (currentPlatform()) {
                 is Platform.Windows -> {
                     // Windows
-                    val command = "explorer /select,\"${file.absolutePath}\""
-                    Runtime.getRuntime().exec(command)
+                    val command = listOf("explorer", "/select", "\"", file.absolutePath, "\"")
+                    ProcessBuilder(command).start()
                     true
                 }
 
                 is Platform.MacOS -> {
                     // macOS
-                    val command = "open -R \"${file.absolutePath}\""
-                    Runtime.getRuntime().exec(command)
+                    val command = listOf("open", "-R", file.absolutePath)
+                    ProcessBuilder(command).start()
                     true
                 }
 
