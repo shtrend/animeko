11e2f3982e095d2897fe0a78946d91280b071a44
Him188 <Him188@mamoe.net>
2024-11-23 13:00:40 +0000

    在 HTTP 请求因 CancellationException 失败时, 只记录为 warn 而不是 error
    方便检索

--- utils/ktor-client/src/commonMain/kotlin/DefaultClient.kt
+++ utils/ktor-client/src/commonMain/kotlin/DefaultClient.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.utils.ktor
 
 import io.ktor.client.HttpClient
@@ -78,8 +87,13 @@ object HttpLogger {
     ) {
         when {
             // 刻意没记录 exception, 因为外面应该会处理
-            responseStatus.isFailure ->
-                error { buildHttpRequestLog(method, url, isAuthorized, responseStatus, duration) }
+            responseStatus.isFailure -> {
+                if (responseStatus.exceptionOrNull() is CancellationException) {
+                    warn { buildHttpRequestLog(method, url, isAuthorized, responseStatus, duration) }
+                } else {
+                    error { buildHttpRequestLog(method, url, isAuthorized, responseStatus, duration) }
+                }
+            }
 
             responseStatus.getOrNull()?.isSuccess() == true ->
                 info { buildHttpRequestLog(method, url, isAuthorized, responseStatus, duration) }
