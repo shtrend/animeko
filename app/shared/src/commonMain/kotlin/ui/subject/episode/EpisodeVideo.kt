e8302010d27168891d6a1af7191dd5a0a3d9d16f
NieR4ever <65274728+NieR4ever@users.noreply.github.com>
2024-11-25 21:21:19 +0800

    添加跳转85秒图标 (#1248)

--- app/shared/src/commonMain/kotlin/ui/subject/episode/EpisodeVideo.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/episode/EpisodeVideo.kt
@@ -32,7 +32,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -66,9 +65,9 @@ import me.him188.ani.app.ui.foundation.LocalPlatform
 import me.him188.ani.app.ui.foundation.TextWithBorder
 import me.him188.ani.app.ui.foundation.effects.cursorVisibility
 import me.him188.ani.app.ui.foundation.icons.AniIcons
+import me.him188.ani.app.ui.foundation.icons.Forward85
 import me.him188.ani.app.ui.foundation.icons.RightPanelClose
 import me.him188.ani.app.ui.foundation.icons.RightPanelOpen
-import me.him188.ani.app.ui.foundation.isInDebugMode
 import me.him188.ani.app.ui.foundation.rememberDebugSettingsViewModel
 import me.him188.ani.app.ui.settings.danmaku.DanmakuRegexFilterState
 import me.him188.ani.app.ui.subject.episode.danmaku.DanmakuEditor
@@ -201,10 +200,8 @@ internal fun EpisodeVideoImpl(
                     null
                 },
                 actions = {
-                    if (isInDebugMode()) {
-                        TextButton({ playerState.skip(85000L) }) {
-                            Text("跳过 85s")
-                        }
+                    IconButton({ playerState.skip(85000L) }) {
+                        Icon(AniIcons.Forward85, "跳过 85s")
                     }
                     if (expanded) {
                         IconButton({ isMediaSelectorVisible = true }, Modifier.testTag(TAG_SHOW_MEDIA_SELECTOR)) {
