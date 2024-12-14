8bae27171059dbdb2d86787c0b60f64d44fb9261
Him188 <Him188@mamoe.net>
2024-12-14 13:29:42 +0000

    Document ExceptionCollector

--- utils/coroutines/src/commonMain/kotlin/ExceptionCollector.kt
+++ utils/coroutines/src/commonMain/kotlin/ExceptionCollector.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.utils.coroutines
 
 import kotlinx.atomicfu.locks.SynchronizedObject
@@ -7,6 +16,15 @@ import kotlin.concurrent.Volatile
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
+/**
+ * 用于收集在进行一个操作之中产生的所有异常 (通常是不那么重要的, 可以忽略的异常),
+ * 如果整个操作最终失败了, 所有中间异常将被 [Throwable.addSuppressed] 合并到最后一个异常, 然后抛出.
+ *
+ * 示例: 假设一个需要重试的操作, 遇到异常后 catch 并重试, 一般的做法是将该异常忽略, 直到重试次数耗尽, 抛出最后一个遇到的异常.
+ * 这会导致 debug 时不知道所有中间异常, 潜在地导致问题难以定位. 使用 [ExceptionCollector] 可以将所有异常收集起来, 在抛出最后异常的同时携带所有中间异常的信息, 帮助 debug.
+ *
+ * @sample me.him188.ani.utils.coroutines.ExceptionCollector_collectOps
+ */
 open class ExceptionCollector {
 
     constructor()
