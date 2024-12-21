1734cf825f6d7f379a6f9ce9e22b52b9808a1be8
Him188 <Him188@mamoe.net>
2024-12-21 20:11:22 +0000

    SelectorMediaSource 只采用第一中文名称搜索
    采用所有名称会搜索太久, 经测试用第一就够了

--- app/shared/app-data/src/commonMain/kotlin/domain/mediasource/web/SelectorMediaSource.kt
+++ app/shared/app-data/src/commonMain/kotlin/domain/mediasource/web/SelectorMediaSource.kt
@@ -214,6 +214,7 @@ class SelectorMediaSource(
         val allSubjectNames = query.subjectNames.toSet()
 
         return query.subjectNames
+            .take(1) // 只采用第一个主名称就够了
             .map { name ->
                 SinglePagePagedSource {
                     engine.search(
