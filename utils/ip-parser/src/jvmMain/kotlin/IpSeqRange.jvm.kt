cc0312a803a738f253c6f8b8b47ddc9152778b9b
Him188 <Him188@mamoe.net>
2024-12-29 23:26:59 +0000

    Do not validate peer IP addresses to improve performance. Fix #1412

--- utils/ip-parser/src/jvmMain/kotlin/IpSeqRange.jvm.kt
+++ utils/ip-parser/src/jvmMain/kotlin/IpSeqRange.jvm.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.utils.ipparser
 
 import inet.ipaddr.AddressStringException
@@ -15,15 +24,29 @@ internal actual fun createIpSeqRange(ipSeqPattern: String): IpSeqRange {
             logger.warn(ex) { "failed to parse ip range $ipSeqPattern" }
             null
         }
-        
+
         override fun contains(address: String): Boolean {
             if (range == null) return false
-            val ipAddress = try { 
-                IPAddressString(address).address 
-            } catch (ex: AddressStringException) { 
+            val ipAddress = try {
+                NoValidationIPAddressString(address).address
+            } catch (_: AddressStringException) {
                 return false
             }
             return range.contains(ipAddress)
         }
+    }
+}
+
+private class NoValidationIPAddressString(addr: String) : IPAddressString(addr) {
+    override fun validate() {
+        // NO-OP
+    }
+
+    override fun validateIPv4() {
+        // NO-OP
+    }
+
+    override fun validateIPv6() {
+        // NO-OP
     }
 }
