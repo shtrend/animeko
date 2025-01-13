81196cd37434f6a2c563021ec53e241347bf681d
Him188 <Him188@mamoe.net>
2025-01-13 10:45:38 +0000

    Stop playback on main thread. fix #1490

--- app/shared/src/commonMain/kotlin/ui/subject/episode/EpisodeViewModel.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/episode/EpisodeViewModel.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -634,7 +634,9 @@ class EpisodeViewModel(
         super.onCleared()
         backgroundScope.launch(NonCancellable + CoroutineName("EpisodeViewModel#onCleared")) {
             fetchPlayState.onClose()
-            player.stopPlayback()
+            withContext(Dispatchers.Main) {
+                player.stopPlayback()
+            }
         }
     }
 
