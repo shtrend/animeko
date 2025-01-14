84723065d6131d7f203620200914eb198070de04
StageGuard <sg@mamoe.net>
2025-01-14 21:07:14 +0800

    Avoid throwing TimeoutCancellationException at `onDestroy` in torrent service.

--- app/shared/app-data/src/androidMain/kotlin/domain/torrent/service/AniTorrentService.kt
+++ app/shared/app-data/src/androidMain/kotlin/domain/torrent/service/AniTorrentService.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -31,7 +31,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
 import kotlinx.io.files.Path
 import me.him188.ani.app.data.models.preference.AnitorrentConfig
 import me.him188.ani.app.data.models.preference.ProxyConfig
@@ -198,7 +198,7 @@ class AniTorrentService : LifecycleService(), CoroutineScope {
             val downloader = engine.getDownloader()
             val sessions = downloader.openSessions.value
 
-            withTimeout(3000L) {
+            withTimeoutOrNull(3000L) {
                 sessions.forEach { (_, session) -> session.close() }
             }
             downloader.close()
