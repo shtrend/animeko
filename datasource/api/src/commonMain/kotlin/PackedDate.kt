63fde7089bf0cfece5643967f26b9bfa5071f4dd
Him188 <Him188@mamoe.net>
2025-01-14 18:46:48 +0000

    PackedDate.parseFromDate: allow redundant components for future extension

--- datasource/api/src/commonMain/kotlin/PackedDate.kt
+++ datasource/api/src/commonMain/kotlin/PackedDate.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -70,7 +70,7 @@ value class PackedDate(
          */
         fun parseFromDate(date: String): PackedDate {
             val split = date.split("-")
-            if (split.size != 3) return Invalid
+            if (split.size < 3) return Invalid
             return PackedDate(
                 split[0].toIntOrNull() ?: return Invalid,
                 split[1].toIntOrNull() ?: return Invalid,
