f75cb5f216626882a9cbc56ef96a1103d4825629
Him188 <Him188@mamoe.net>
2025-01-14 00:28:38 +0000

    [NFC] Clarify TODO in RememberPlayProgressExtensionTest

--- app/shared/app-data/src/commonTest/kotlin/domain/player/extension/RememberPlayProgressExtensionTest.kt
+++ app/shared/app-data/src/commonTest/kotlin/domain/player/extension/RememberPlayProgressExtensionTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -205,7 +205,7 @@ class RememberPlayProgressExtensionTest : AbstractPlayerExtensionTest() {
     }
 
     @Test
-    @Ignore // TODO: We should enable this test
+    @Ignore // TODO: This behavior is currently not implemented. We should implement according to the test.
     fun `when stopPlayback at 1 percent - saves play progress`() = runTest {
         val (testScope, suite, state) = createCase()
         advanceUntilIdle()
@@ -225,7 +225,7 @@ class RememberPlayProgressExtensionTest : AbstractPlayerExtensionTest() {
     }
 
     @Test
-    @Ignore // TODO: We should enable this test
+    @Ignore // TODO: This behavior is currently not implemented. We should implement according to the test.
     fun `when stopPlayback at end - removes play progress`() = runTest {
         val (testScope, suite, state) = createCase()
         advanceUntilIdle()
