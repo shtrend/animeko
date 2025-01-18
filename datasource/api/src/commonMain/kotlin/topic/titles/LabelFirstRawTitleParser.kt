2f36e9d77d308947e02a6cc52f1a14b322b4417f
Him188 <Him188@mamoe.net>
2025-01-18 00:06:43 +0000

    L1 识别"无中文字幕", close #1515

--- datasource/api/src/commonMain/kotlin/topic/titles/LabelFirstRawTitleParser.kt
+++ datasource/api/src/commonMain/kotlin/topic/titles/LabelFirstRawTitleParser.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -95,6 +95,11 @@ class LabelFirstRawTitleParser : RawTitleParser() {
                     builder.subtitleLanguages.count { it != SubtitleLanguage.Japanese } >= 2 -> SubtitleKind.CLOSED
                     else -> null
                 }
+            }
+
+            if (words.any { it == "无中文字幕" }) {
+                builder.subtitleLanguages.clear()
+                builder.subtitleKind = null
             }
         }
         return
