70dbbeadf2e7b653d71fae9f104f12dfba8b400a
Him188 <Him188@mamoe.net>
2025-01-19 02:08:29 +0000

    Enable android test for datasource-api

--- datasource/api/src/commonTest/kotlin/paging/PagedSourceTest.kt
+++ datasource/api/src/commonTest/kotlin/paging/PagedSourceTest.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024-2025 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.datasources.api.paging
 
 import kotlinx.coroutines.flow.flowOf
@@ -57,7 +66,7 @@ abstract class PagedSourceTest {
     }
 
     @Test
-    fun `don't finish after first page`() = runTest {
+    fun `dont finish after first page`() = runTest {
         val source = createSource()
 
         assertEquals(listOf(1, 2, 3), source.nextPageOrNull())
