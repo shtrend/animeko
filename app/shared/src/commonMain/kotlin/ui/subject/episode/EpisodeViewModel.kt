9be0a9f5256c27a52d56b62f7ed4916e723b33bf
StageGuard <sg@mamoe.net>
2025-01-30 18:50:37 +0800

    Fix danmaku not repopulating on Android, close #1599.

--- app/shared/src/commonMain/kotlin/ui/subject/episode/EpisodeViewModel.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/episode/EpisodeViewModel.kt
@@ -453,7 +453,9 @@ class EpisodeViewModel(
                         event.list
                             .filter { it.text.any { c -> !c.isWhitespace() } }
                             .map { createDanmakuPresentation(it, selfId) },
-                        player.getCurrentPositionMillis(),
+                        withContext(Dispatchers.Main) {
+                            player.getCurrentPositionMillis()
+                        },
                     )
                 }
             }
