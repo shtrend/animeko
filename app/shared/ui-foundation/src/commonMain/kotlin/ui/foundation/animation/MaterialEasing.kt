61da6acf16357d15632f82fc550db46db8741820
Him188 <Him188@mamoe.net>
2025-02-01 14:29:07 +0000

    Minor: Mark easing specs with `@Stable`

--- app/shared/ui-foundation/src/commonMain/kotlin/ui/foundation/animation/MaterialEasing.kt
+++ app/shared/ui-foundation/src/commonMain/kotlin/ui/foundation/animation/MaterialEasing.kt
@@ -11,24 +11,31 @@ package me.him188.ani.app.ui.foundation.animation
 
 import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.Easing
+import androidx.compose.runtime.Stable
 
 // https://m3.material.io/styles/motion/easing-and-duration/tokens-specs#433b1153-2ea3-4fe2-9748-803a47bc97ee
 
 // md.sys.motion.easing.standard
+@Stable
 val StandardEasing: Easing = CubicBezierEasing(0.2f, 0.0f, 0f, 1.0f)
 
 // md.sys.motion.easing.standard.decelerate
+@Stable
 val StandardDecelerateEasing: Easing = CubicBezierEasing(0.0f, 0.0f, 0.0f, 1f)
 
 // md.sys.motion.easing.standard.accelerate
+@Stable
 val StandardAccelerateEasing: Easing = CubicBezierEasing(0.3f, 0.0f, 1f, 1f)
 
 // TODO: EmphasizedEasing should actually be pathInterpolator(M 0,0 C 0.05, 0, 0.133333, 0.06, 0.166666, 0.4 C 0.208333, 0.82, 0.25, 1, 1, 1)
 // md.sys.motion.easing.emphasized
+@Stable
 val EmphasizedEasing: Easing = StandardEasing
 
 // md.sys.motion.easing.emphasized.decelerate
+@Stable
 val EmphasizedDecelerateEasing: Easing = CubicBezierEasing(0.05f, 0.7f, 0.1f, 1.0f)
 
 // md.sys.motion.easing.emphasized.accelerate
+@Stable
 val EmphasizedAccelerateEasing: Easing = CubicBezierEasing(0.3f, 0.0f, 0.8f, 0.15f)
