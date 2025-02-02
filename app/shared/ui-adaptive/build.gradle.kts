ac14bcd93bd07458844b1e86e14ee6c3e86e499f
Him188 <Him188@mamoe.net>
2025-02-02 20:38:10 +0000

    [build] Add explicit androidx-compose-material3-adaptive for IDE reference

--- app/shared/ui-adaptive/build.gradle.kts
+++ app/shared/ui-adaptive/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -48,6 +48,9 @@ kotlin {
         api(libs.compose.material3.adaptive.core.get().toString()) {
             exclude("androidx.window.core", "window-core")
         }
+        api(libs.androidx.compose.material3.adaptive)
+        api(libs.androidx.compose.material3.adaptive.layout)
+        api(libs.androidx.compose.material3.adaptive.navigation)
         // Preview only
     }
 }
