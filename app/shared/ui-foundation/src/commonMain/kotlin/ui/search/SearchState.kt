e56f9d95244eb122da14cb1ae7f607aac1c2de50
Him188 <Him188@mamoe.net>
2025-03-09 14:24:10 +0000

    Fix potentially not cached pagers
    which caused crashes

--- app/shared/ui-foundation/src/commonMain/kotlin/ui/search/SearchState.kt
+++ app/shared/ui-foundation/src/commonMain/kotlin/ui/search/SearchState.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -19,10 +19,13 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.paging.LoadState
 import androidx.paging.LoadStates
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItemsWithLifecycle
 import androidx.paging.compose.launchAsLazyPagingItemsIn
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -102,6 +105,7 @@ fun <T : Any> SearchState<T>.collectHasQueryAsState(): State<Boolean> {
     }
 }
 
+@OptIn(DelicateCoroutinesApi::class)
 @Stable
 private val emptyPager: Flow<PagingData<Any>> = flowOf(
     PagingData.from(
@@ -112,7 +116,7 @@ private val emptyPager: Flow<PagingData<Any>> = flowOf(
             LoadState.NotLoading(endOfPaginationReached = true),
         ),
     ),
-)
+).cachedIn(GlobalScope)
 
 
 @Stable
