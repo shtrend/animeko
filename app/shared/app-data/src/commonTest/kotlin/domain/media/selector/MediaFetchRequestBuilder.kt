288de2b9a1d81089ff9ad9d791316460f331ea0b
Him188 <Him188@mamoe.net>
2025-03-20 19:10:20 +0000

    [test] Add MediaFetchRequestBuilder for MediaSelector-related tests

---
+++ app/shared/app-data/src/commonTest/kotlin/domain/media/selector/MediaFetchRequestBuilder.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2024-2025 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
+package me.him188.ani.app.domain.media.selector
+
+import me.him188.ani.datasources.api.EpisodeSort
+import me.him188.ani.datasources.api.source.MediaFetchRequest
+
+/**
+ * @see me.him188.ani.datasources.api.source.MediaFetchRequest
+ */
+class MediaFetchRequestBuilder(
+    var subjectId: String = "1",
+    var episodeId: String = "1",
+    var subjectNameCN: String? = null,
+    var subjectNames: List<String> = listOf(),
+    var episodeSort: EpisodeSort = EpisodeSort(1),
+    var episodeName: String = "",
+    var episodeEp: EpisodeSort? = null,
+) {
+    fun build(): MediaFetchRequest = MediaFetchRequest(
+        subjectId,
+        episodeId,
+        subjectNameCN,
+        subjectNames,
+        episodeSort,
+        episodeName,
+        episodeEp,
+    )
+}
+
+inline fun buildMediaFetchRequest(
+    action: MediaFetchRequestBuilder.() -> Unit
+): MediaFetchRequest {
+    return MediaFetchRequestBuilder().apply(action).build()
+}
