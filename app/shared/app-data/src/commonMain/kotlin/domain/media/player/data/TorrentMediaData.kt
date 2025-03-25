5440390bd04bbcc5cafaa85798ac46a36e242f2b
Him188 <Him188@mamoe.net>
2025-03-25 01:38:46 +0000

    Update mediamp to 0.0.22

--- app/shared/app-data/src/commonMain/kotlin/domain/media/player/data/TorrentMediaData.kt
+++ app/shared/app-data/src/commonMain/kotlin/domain/media/player/data/TorrentMediaData.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -24,6 +24,7 @@ class TorrentMediaData(
     private val handle: TorrentFileHandle,
     private val onClose: () -> Unit,
     override val extraFiles: MediaExtraFiles = MediaExtraFiles.EMPTY,
+    override val options: List<String> = emptyList(),
 ) : SeekableInputMediaData, DownloadingMediaData, FileMediaData {
     private inline val entry get() = handle.entry
     override val filename: String get() = entry.pathInTorrent
