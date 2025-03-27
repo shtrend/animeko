0d563cc07a54f286d88ec38184b287fd4d032681
Him188 <Him188@mamoe.net>
2025-03-27 03:26:23 +0000

    Ensure logging charset to be UTF-8

--- app/desktop/src/main/kotlin/Log4j2Config.kt
+++ app/desktop/src/main/kotlin/Log4j2Config.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024-2025 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.desktop
 
 import org.apache.logging.log4j.Level
@@ -18,11 +27,13 @@ object Log4j2Config {
         val builder: ConfigurationBuilder<BuiltConfiguration> = ConfigurationBuilderFactory.newConfigurationBuilder()
 
         // Console appender
+        val charset = "UTF-8"
         val consoleLayoutBuilder: LayoutComponentBuilder = builder.newLayout("PatternLayout")
             .addAttribute(
                 "pattern",
                 "%highlight{%d [%-5level] %c{1}: %msg%n%throwable}{FATAL=red bold, ERROR=red, WARN=yellow, INFO=transparent, DEBUG=bright_blue, TRACE=bright_green}",
             )
+            .addAttribute("charset", charset) // Specify charset
         val consoleAppenderBuilder: AppenderComponentBuilder = builder.newAppender("STDOUT", "Console")
             .add(consoleLayoutBuilder)
         builder.add(consoleAppenderBuilder)
@@ -30,10 +41,12 @@ object Log4j2Config {
         // File appender
         val fileLayoutBuilder: LayoutComponentBuilder = builder.newLayout("PatternLayout")
             .addAttribute("pattern", "%d [%-5level] %c: %msg%n%throwable")
+            .addAttribute("charset", charset) // Specify charset
         val fileAppenderBuilder: AppenderComponentBuilder = builder.newAppender("FILE", "RollingFile")
             .addAttribute("fileName", "${logsFolder.absolutePath}/app.log")
             .addAttribute("filePattern", "${logsFolder.absolutePath}/app-%d{yyyy-MM-dd}.log")
             .add(fileLayoutBuilder)
+
         fileAppenderBuilder.addComponent(
             builder.newComponent("Policies")
                 .addComponent(
@@ -66,6 +79,7 @@ object Log4j2Config {
             builder.newLogger("org.openqa.selenium", Level.OFF)
                 .addAttribute("additivity", false),
         )
+
         val ctx: LoggerContext = LogManager.getContext(false) as LoggerContext
         val config: Configuration = builder.build()
         ctx.start(config)
