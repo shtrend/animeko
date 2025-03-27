9781e583eb4ee5b850f6d85cda306accc4c34a1b
Him188 <Him188@mamoe.net>
2025-03-27 01:15:34 +0000

    Automatically delete old JCEF logs, close #1831

--- app/shared/application/src/jvmMain/kotlin/platform/JvmLogHelper.kt
+++ app/shared/application/src/jvmMain/kotlin/platform/JvmLogHelper.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024-2025 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.platform
 
 import kotlinx.datetime.Clock
@@ -21,7 +30,7 @@ object JvmLogHelper {
                 listDirectoryEntries()
             } else emptyList()
         }.forEach { file ->
-            if (file.extension == "log" && file.name.startsWith("app")
+            if (file.extension == "log" && (file.name.startsWith("app") || file.name.startsWith("cef-"))
                 && now - file.getLastModifiedTime().toInstant().toKotlinInstant() > 3.days
             ) {
                 file.deleteIfExists()
