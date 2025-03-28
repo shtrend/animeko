e6e4378240ed0662fce2d81f8d3358061ff23d88
Him188 <Him188@mamoe.net>
2025-03-28 19:46:39 +0000

    [ios] Fix Context.ios

--- app/shared/app-platform/src/iosMain/kotlin/platform/Context.ios.kt
+++ app/shared/app-platform/src/iosMain/kotlin/platform/Context.ios.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024-2025 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.platform
 
 import androidx.compose.runtime.ProvidableCompositionLocal
@@ -20,7 +29,7 @@ fun Context.asIosContext(): IosContext {
     return this as IosContext
 }
 
-internal actual val Context.filesImpl: ContextFiles get() = files
+internal actual val Context.filesImpl: ContextFiles get() = this.asIosContext().files
 
 class IosContextFiles(
     override val cacheDir: SystemPath,
