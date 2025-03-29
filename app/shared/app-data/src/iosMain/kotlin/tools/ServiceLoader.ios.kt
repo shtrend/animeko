ac3ff362be307089c2a3ee55426b981cc50e2a14
Him188 <Him188@mamoe.net>
2025-03-29 13:40:43 +0000

    [ios] Implement ServiceLoader as emptyList

--- app/shared/app-data/src/iosMain/kotlin/tools/ServiceLoader.ios.kt
+++ app/shared/app-data/src/iosMain/kotlin/tools/ServiceLoader.ios.kt
@@ -1,15 +1,19 @@
+/*
+ * Copyright (C) 2024-2025 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.tools
 
-import me.him188.ani.app.torrent.anitorrent.AnitorrentDownloaderFactory
-import me.him188.ani.app.torrent.api.TorrentDownloaderFactory
-import me.him188.ani.datasources.api.source.MediaSourceFactory
-import me.him188.ani.datasources.mikan.MikanCNMediaSource
-import me.him188.ani.datasources.mikan.MikanMediaSource
 import kotlin.reflect.KClass
 
 actual object ServiceLoader {
     actual fun <T : Any> loadServices(kClass: KClass<T>): List<T> {
-        TODO("IOS ServiceLoader")
+        return emptyList()
     }
 }
 
