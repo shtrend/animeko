650e29daba94e3c2d60636795adeddc7de66ae02
Him188 <Him188@mamoe.net>
2025-04-11 15:12:20 +0100

    [NFC] Move MediaSelectorHelp previews from androidMain to commonMain

--- app/shared/src/commonMain/kotlin/ui/subject/episode/mediaFetch/MediaSelectorHelp.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/episode/mediaFetch/MediaSelectorHelp.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024-2025 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.ui.subject.episode.mediaFetch
 
 import androidx.compose.foundation.layout.Arrangement
@@ -16,8 +25,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import me.him188.ani.app.ui.foundation.ProvideCompositionLocalsForPreview
 import me.him188.ani.app.ui.foundation.widgets.RichDialogLayout
 import me.him188.ani.app.ui.settings.rendering.MediaSourceIcons
+import org.jetbrains.compose.ui.tooling.preview.PreviewLightDark
 
 
 @Composable
@@ -87,5 +98,14 @@ fun ExplainerCard(
                 content()
             }
         }
+    }
+}
+
+
+@PreviewLightDark
+@Composable
+private fun PreviewMediaSelectorHelp() {
+    ProvideCompositionLocalsForPreview {
+        MediaSelectorHelp({})
     }
 }
