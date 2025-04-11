8741eff74bf13968a7c8973e16af756c2ca2c990
Him188 <Him188@mamoe.net>
2025-04-11 15:11:44 +0100

    [NFC] Move MediaSelector previews from androidMain to commonMain

--- app/shared/src/commonMain/kotlin/ui/subject/episode/mediaFetch/MediaSelectorView.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/episode/mediaFetch/MediaSelectorView.kt
@@ -56,16 +56,33 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
+import me.him188.ani.app.data.models.preference.MediaPreference
+import me.him188.ani.app.data.models.preference.MediaSelectorSettings
+import me.him188.ani.app.domain.media.TestMediaList
+import me.him188.ani.app.domain.media.cache.MediaCacheManager
+import me.him188.ani.app.domain.media.selector.DefaultMediaSelector
+import me.him188.ani.app.domain.media.selector.MaybeExcludedMedia
+import me.him188.ani.app.domain.media.selector.MediaExclusionReason
+import me.him188.ani.app.domain.media.selector.MediaSelectorContext
+import me.him188.ani.app.domain.media.selector.TestMatchMetadata
 import me.him188.ani.app.domain.media.selector.UnsafeOriginalMediaAccess
 import me.him188.ani.app.platform.currentAniBuildConfig
+import me.him188.ani.app.ui.foundation.ProvideCompositionLocalsForPreview
 import me.him188.ani.app.ui.foundation.animation.LocalAniMotionScheme
 import me.him188.ani.app.ui.foundation.ifThen
 import me.him188.ani.app.ui.mediaselect.selector.MediaSelectorWebSourcesColumn
+import me.him188.ani.datasources.api.CachedMedia
 import me.him188.ani.datasources.api.Media
+import me.him188.ani.datasources.api.topic.ResourceLocation
+import me.him188.ani.utils.platform.annotations.TestOnly
+import org.jetbrains.compose.ui.tooling.preview.PreviewLightDark
 
 
 private inline val WINDOW_VERTICAL_PADDING get() = 8.dp
@@ -334,3 +351,119 @@ private fun LazyItemScope.MediaItemGroup(
     }
 }
 
+
+///////////////////////////////////////////////////////////////////////////
+// Previews
+///////////////////////////////////////////////////////////////////////////
+
+
+@TestOnly
+internal val previewMediaList = TestMediaList.run {
+    listOf(
+        CachedMedia(
+            origin = this[0],
+            cacheMediaSourceId = MediaCacheManager.LOCAL_FS_MEDIA_SOURCE_ID,
+            download = ResourceLocation.LocalFile("file://test.txt"),
+        ),
+    ) + this
+}
+
+@OptIn(TestOnly::class)
+@PreviewLightDark
+@Composable
+private fun PreviewMediaSelector() {
+    val scope = rememberCoroutineScope()
+    val mediaSelector = rememberTestMediaSelectorPresentation(previewMediaList, scope)
+    ProvideCompositionLocalsForPreview {
+        Surface {
+            val (viewKind, onViewKindChange) = rememberSaveable { mutableStateOf(ViewKind.WEB) }
+            MediaSelectorView(
+                state = mediaSelector,
+                viewKind = viewKind,
+                onViewKindChange = onViewKindChange,
+                sourceResults = {
+                    MediaSourceResultsView(
+                        TestMediaSourceResultListPresentation,
+                        mediaSelector,
+                        onRefresh = {},
+                        onRestartSource = {},
+                    )
+                },
+                onRestartSource = {
+                },
+            )
+        }
+    }
+}
+
+@Composable
+@OptIn(TestOnly::class)
+private fun rememberTestMediaSelectorPresentation(previewMediaList: List<Media>, scope: CoroutineScope) =
+    rememberMediaSelectorState(
+        rememberTestMediaSourceInfoProvider(),
+        createTestMediaSourceResultsFilterer(scope).filteredSourceResults,
+    ) {
+        DefaultMediaSelector(
+            mediaSelectorContextNotCached = flowOf(MediaSelectorContext.EmptyForPreview),
+            mediaListNotCached = MutableStateFlow(
+                listOf(
+                    CachedMedia(
+                        origin = previewMediaList[0],
+                        cacheMediaSourceId = MediaCacheManager.LOCAL_FS_MEDIA_SOURCE_ID,
+                        download = ResourceLocation.LocalFile("file://test.txt"),
+                    ),
+                ) + previewMediaList,
+            ),
+            savedUserPreference = flowOf(MediaPreference.Empty),
+            savedDefaultPreference = flowOf(
+                MediaPreference.PlatformDefault.copy(
+                    subtitleLanguageId = "CHS",
+                ),
+            ),
+            mediaSelectorSettings = flowOf(MediaSelectorSettings.AllVisible),
+        )
+    }
+
+@OptIn(TestOnly::class)
+@PreviewLightDark
+@Composable
+private fun PreviewMediaItemIncluded(modifier: Modifier = Modifier) = ProvideCompositionLocalsForPreview {
+    MediaSelectorItem(
+        remember {
+            MediaGroupBuilder("Test").apply {
+                add(previewMediaList[0].let { MaybeExcludedMedia.Included(it, TestMatchMetadata) })
+            }.build()
+        },
+        remember { MediaGroupState("test") },
+        rememberTestMediaSourceInfoProvider(),
+        selected = false,
+        onSelect = {},
+        preferredResolution = { null },
+        onPreferResolution = {},
+        preferredSubtitleLanguageId = { null },
+        onPreferSubtitleLanguageId = {},
+        modifier = modifier,
+    )
+}
+
+@OptIn(TestOnly::class)
+@PreviewLightDark
+@Composable
+private fun PreviewMediaItemExcluded(modifier: Modifier = Modifier) = ProvideCompositionLocalsForPreview {
+    MediaSelectorItem(
+        remember {
+            MediaGroupBuilder("Test").apply {
+                add(previewMediaList[0].let { MaybeExcludedMedia.Excluded(it, MediaExclusionReason.FromSequelSeason) })
+            }.build()
+        },
+        remember { MediaGroupState("test") },
+        rememberTestMediaSourceInfoProvider(),
+        selected = false,
+        onSelect = {},
+        preferredResolution = { null },
+        onPreferResolution = {},
+        preferredSubtitleLanguageId = { null },
+        onPreferSubtitleLanguageId = {},
+        modifier = modifier,
+    )
+}
