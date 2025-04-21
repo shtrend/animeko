2b2b8cf83a00fcfa67eb79725b03ba0a16e59e67
Him188 <Him188@mamoe.net>
2025-04-21 13:02:51 +0100

    Move EpisodeList previews from androidMain to commonMain

--- app/shared/ui-subject/src/commonMain/kotlin/ui/subject/episode/list/EpisodeList.kt
+++ app/shared/ui-subject/src/commonMain/kotlin/ui/subject/episode/list/EpisodeList.kt
@@ -39,6 +39,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -53,7 +54,9 @@ import me.him188.ani.app.data.models.preference.EpisodeListProgressTheme
 import me.him188.ani.app.domain.media.cache.EpisodeCacheStatus
 import me.him188.ani.app.navigation.LocalNavigator
 import me.him188.ani.app.platform.LocalContext
+import me.him188.ani.app.tools.toProgress
 import me.him188.ani.app.ui.foundation.FilledTonalCombinedClickButton
+import me.him188.ani.app.ui.foundation.ProvideCompositionLocalsForPreview
 import me.him188.ani.app.ui.foundation.indication.HorizontalIndicator
 import me.him188.ani.app.ui.foundation.indication.IndicatedBox
 import me.him188.ani.app.ui.foundation.interaction.VibrationStrength
@@ -61,7 +64,11 @@ import me.him188.ani.app.ui.foundation.interaction.vibrateIfSupported
 import me.him188.ani.app.ui.foundation.theme.stronglyWeaken
 import me.him188.ani.app.ui.foundation.theme.weaken
 import me.him188.ani.app.ui.subject.collection.progress.EpisodeListState
+import me.him188.ani.datasources.api.EpisodeSort
+import me.him188.ani.datasources.api.PackedDate
+import me.him188.ani.datasources.api.topic.FileSize.Companion.megaBytes
 import me.him188.ani.datasources.api.topic.UnifiedCollectionType
+import org.jetbrains.compose.ui.tooling.preview.PreviewLightDark
 
 
 /**
@@ -311,3 +318,156 @@ fun cacheStatusIndicationColor(
         else -> Color.Transparent
     }
 }
+
+///////////////////////////////////////////////////////////////////////////
+// Previews
+///////////////////////////////////////////////////////////////////////////
+
+
+private val airDate = PackedDate(2021, 1, 1)
+
+private val testEpisodes = listOf(
+    EpisodeProgressItem(
+        episodeId = 0,
+        episodeSort = "00",
+        collectionType = UnifiedCollectionType.DONE,
+        isOnAir = false,
+        cacheStatus = EpisodeCacheStatus.Caching(0.3f.toProgress(), 300.megaBytes),
+    ),
+    EpisodeProgressItem(
+        episodeId = 1,
+        episodeSort = "01",
+        collectionType = UnifiedCollectionType.DONE,
+        isOnAir = false,
+        cacheStatus = EpisodeCacheStatus.NotCached,
+    ),
+    EpisodeProgressItem(
+        episodeId = 2,
+        episodeSort = "02",
+        collectionType = UnifiedCollectionType.DONE,
+        isOnAir = false,
+        cacheStatus = EpisodeCacheStatus.Cached(300.megaBytes),
+    ),
+    EpisodeProgressItem(
+        episodeId = 3,
+        episodeSort = "03",
+        collectionType = UnifiedCollectionType.WISH,
+        isOnAir = false,
+        cacheStatus = EpisodeCacheStatus.Cached(300.megaBytes),
+    ),
+    EpisodeProgressItem(
+        episodeId = 4,
+        episodeSort = "04",
+        collectionType = UnifiedCollectionType.WISH,
+        isOnAir = false,
+        cacheStatus = EpisodeCacheStatus.Caching(0.7f.toProgress(), 300.megaBytes),
+    ),
+    EpisodeProgressItem(
+        episodeId = 5,
+        episodeSort = "05",
+        collectionType = UnifiedCollectionType.WISH,
+        isOnAir = false,
+        cacheStatus = EpisodeCacheStatus.NotCached,
+    ),
+    EpisodeProgressItem(
+        episodeId = 6,
+        episodeSort = "06",
+        collectionType = UnifiedCollectionType.WISH,
+        isOnAir = true,
+        cacheStatus = EpisodeCacheStatus.NotCached,
+    ),
+    EpisodeProgressItem(
+        episodeId = 7,
+        episodeSort = "07",
+        collectionType = UnifiedCollectionType.WISH,
+        isOnAir = true,
+        cacheStatus = EpisodeCacheStatus.Cached(300.megaBytes),
+    ),
+    EpisodeProgressItem(
+        episodeId = 8,
+        episodeSort = "08",
+        collectionType = UnifiedCollectionType.WISH,
+        isOnAir = true,
+        cacheStatus = EpisodeCacheStatus.Caching(0.3f.toProgress(), 300.megaBytes),
+    ),
+)
+
+@PreviewLightDark
+@Composable
+private fun PreviewEpisodeProgressDialog() {
+    ProvideCompositionLocalsForPreview {
+        EpisodeListDialog(
+            onDismissRequest = {},
+            title = { Text(text = "葬送的芙莉莲") },
+            onClickCache = {},
+        ) {
+            val episodes = remember {
+                testEpisodes
+            }
+            EpisodeListFlowRow(
+                episodes = {
+                    episodes
+                },
+                onClickEpisodeState = {},
+                onLongClickEpisode = {},
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun PreviewEpisodeProgressDialogLightUp() {
+    ProvideCompositionLocalsForPreview {
+        EpisodeListDialog(
+            onDismissRequest = {},
+            title = { Text(text = "葬送的芙莉莲") },
+            onClickCache = {},
+        ) {
+            val episodes = remember {
+                testEpisodes
+            }
+            EpisodeListFlowRow(
+                episodes = {
+                    episodes
+                },
+                onClickEpisodeState = {},
+                onLongClickEpisode = {},
+                colors = EpisodeListDefaults.colors(EpisodeListProgressTheme.LIGHT_UP),
+            )
+        }
+    }
+}
+
+// 特别长需要限制高度并且滚动, #182
+@PreviewLightDark
+@Composable
+private fun PreviewEpisodeProgressDialogVeryLong() {
+    ProvideCompositionLocalsForPreview {
+        EpisodeListDialog(
+            onDismissRequest = {},
+            title = { Text(text = "银魂") },
+            onClickCache = {},
+        ) {
+            val episodes = remember {
+                // 数字太大 preview 会很卡
+                (0..70).map { item(it) }
+            }
+            EpisodeListFlowRow(
+                episodes = {
+                    episodes
+                },
+                onClickEpisodeState = {},
+                onLongClickEpisode = {},
+            )
+        }
+    }
+}
+
+private fun item(id: Int) = EpisodeProgressItem(
+    episodeId = id,
+    episodeSort = id.toString(),
+    collectionType = UnifiedCollectionType.WISH,
+    isOnAir = true,
+    cacheStatus = EpisodeCacheStatus.Caching(0.3f.toProgress(), 300.megaBytes),
+)
