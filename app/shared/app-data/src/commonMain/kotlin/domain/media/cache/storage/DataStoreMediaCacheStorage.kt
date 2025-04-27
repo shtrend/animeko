0a67e6552a0b20150a3cd52e46d3794ea2a40a8f
StageGuard <sg@mamoe.net>
2025-04-27 23:25:59 +0800

    fixup! fixup! Fix import.

--- app/shared/app-data/src/commonMain/kotlin/domain/media/cache/storage/DataStoreMediaCacheStorage.kt
+++ app/shared/app-data/src/commonMain/kotlin/domain/media/cache/storage/DataStoreMediaCacheStorage.kt
@@ -67,6 +67,7 @@ import me.him188.ani.utils.logging.logger
 import me.him188.ani.utils.logging.warn
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.jvm.Volatile
 
 /**
  * 本地目录缓存, 管理本地目录以及元数据的存储, 调用 [MediaCacheEngine] 进行缓存的实际创建
