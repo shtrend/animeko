337a0cf5981bbb710c80e91ed7692af6e42a6706
Him188 <Him188@mamoe.net>
2025-05-09 14:12:38 +0100

    Remove verbose log in requestService

--- app/shared/app-data/src/androidMain/kotlin/domain/torrent/service/TorrentServiceConnectionManager.kt
+++ app/shared/app-data/src/androidMain/kotlin/domain/torrent/service/TorrentServiceConnectionManager.kt
@@ -47,7 +47,6 @@ import me.him188.ani.app.domain.torrent.IRemoteAniTorrentEngine
 import me.him188.ani.app.domain.torrent.TorrentEngineType
 import me.him188.ani.utils.coroutines.childScope
 import me.him188.ani.utils.coroutines.update
-import me.him188.ani.utils.logging.debug
 import me.him188.ani.utils.logging.info
 import me.him188.ani.utils.logging.logger
 import me.him188.ani.utils.logging.warn
@@ -146,9 +145,6 @@ class TorrentServiceConnectionManager(
 
     @UnsafeTorrentEngineAccessApi
     override fun requestService(token: Any, use: Boolean): Boolean {
-        logger.debug(Exception("show stacktrace")) {
-            "Request ${if (use) "use" else "release"} torrent engine with token $token"
-        }
         requestQueue.update {
             if (use) {
                 add(token)
