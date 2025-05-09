bd6b78cad805b08355a81c959bfc8c1ea1edb6b1
StageGuard <sg@mamoe.net>
2025-05-09 15:00:13 +0800

    Update settings.gradle.kts
    Update mediamp to 0.0.28

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -40,7 +40,7 @@ dependencyResolutionManagement {
         }
 
         create("mediampLibs") {
-            from("org.openani.mediamp:catalog:0.0.27")
+            from("org.openani.mediamp:catalog:0.0.28")
         }
     }
 }
