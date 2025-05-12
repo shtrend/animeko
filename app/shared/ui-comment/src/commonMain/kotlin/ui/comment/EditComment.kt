1cbc969407a246bb1b036dfd64011e35808512b7
Him188 <Him188@mamoe.net>
2025-05-12 12:36:35 +0100

    增加发评论去向提示

--- app/shared/ui-comment/src/commonMain/kotlin/ui/comment/EditComment.kt
+++ app/shared/ui-comment/src/commonMain/kotlin/ui/comment/EditComment.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -240,7 +240,14 @@ fun EditCommentScaffold(
                     onClick = { onClickExpand(!expanded) },
                 )
             }
+        }
 
+        Row(Modifier.padding(horizontal = 8.dp)) {
+            Text(
+                "评论将发送到 Bangumi，请勿讨论视频播放问题",
+                style = MaterialTheme.typography.labelLarge,
+                color = MaterialTheme.colorScheme.outline,
+            )
         }
 
         ProvideContentColor(contentColor) {
