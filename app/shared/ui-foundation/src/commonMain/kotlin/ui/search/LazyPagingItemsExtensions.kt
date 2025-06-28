960c6b7d43ac49a5b1fef34930895a9535342399
Him188 <Him188@mamoe.net>
2025-06-28 15:38:00 +0100

    Add LazyListExtensions

--- app/shared/ui-foundation/src/commonMain/kotlin/ui/search/LazyPagingItemsExtensions.kt
+++ app/shared/ui-foundation/src/commonMain/kotlin/ui/search/LazyPagingItemsExtensions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -28,11 +28,11 @@ val LazyPagingItems<*>.isLoadingFirstPageOrRefreshing: Boolean
 
 @Stable
 val LazyPagingItems<*>.isLoadingFirstOrNextPage: Boolean
-    get() = loadState.append is LoadState.Loading
+    get() = isLoadingFirstPage || isLoadingNextPage
 
 @Stable
 val LazyPagingItems<*>.isLoadingNextPage: Boolean
-    get() = isLoadingFirstOrNextPage
+    get() = loadState.append is LoadState.Loading
 
 @Stable
 val LazyPagingItems<*>.hasFirstPage: Boolean
