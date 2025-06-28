049e9a269197c646084ed6d1b8708c0b0bcbd272
Him188 <Him188@mamoe.net>
2025-06-28 02:06:11 +0100

    为设置页的 detail pane 增加 1000dp 最大宽度

--- app/shared/ui-settings/src/commonMain/kotlin/ui/settings/SettingsScreen.kt
+++ app/shared/ui-settings/src/commonMain/kotlin/ui/settings/SettingsScreen.kt
@@ -24,6 +24,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -492,7 +494,10 @@ internal fun SettingsPageLayout(
                     Column(
                         Modifier
                             .verticalScroll(rememberScrollState())
-                            .padding(horizontal = SettingsScope.itemExtraHorizontalPadding),
+                            .padding(horizontal = SettingsScope.itemExtraHorizontalPadding)
+                            .fillMaxWidth()
+                            .wrapContentWidth()
+                            .widthIn(max = 1000.dp),
                     ) {
                         scope.content()
                     }
