21afc8447682fe7a44c4780edcc7b6235c2ec9e3
StageGuard <sg@mamoe.net>
2025-07-26 10:43:17 +0800

    Wrap data of proto converters.

--- app/shared/app-data/src/commonMain/kotlin/data/persistent/database/ProtoConverters.kt
+++ app/shared/app-data/src/commonMain/kotlin/data/persistent/database/ProtoConverters.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -10,14 +10,19 @@
 package me.him188.ani.app.data.persistent.database
 
 import androidx.room.TypeConverter
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.IntArraySerializer
-import kotlinx.serialization.builtins.ListSerializer
-import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.protobuf.ProtoBuf
 import me.him188.ani.app.data.models.subject.Tag
 import me.him188.ani.datasources.api.EpisodeSort
 
-private val protobuf get() = ProtoBuf.Default
+private val protobuf
+    get() = ProtoBuf {
+        serializersModule = SerializersModule {
+            contextual(IntArray::class, IntArraySerializer())
+        }
+    }
 
 interface ProtoConverter<T> {
     @TypeConverter
@@ -29,50 +34,63 @@ interface ProtoConverter<T> {
 
 object ProtoConverters {
     object StringList : ProtoConverter<List<String>> {
+        @Serializable
+        private class Node(val value: List<String>)
+
         @TypeConverter
         override fun fromByteArray(value: ByteArray): List<String> {
-            return protobuf.decodeFromByteArray(ListSerializer(String.serializer()), value)
+            return protobuf.decodeFromByteArray(Node.serializer(), value).value
         }
 
         @TypeConverter
         override fun fromList(list: List<String>): ByteArray {
-            return protobuf.encodeToByteArray(ListSerializer(String.serializer()), list)
+            return protobuf.encodeToByteArray(Node.serializer(), Node(list))
         }
     }
 
     object IntList : ProtoConverter<List<Int>> {
+        @Serializable
+        private class Node(val value: List<Int>)
+
+
         @TypeConverter
         override fun fromByteArray(value: ByteArray): List<Int> {
-            return protobuf.decodeFromByteArray(ListSerializer(Int.serializer()), value)
+            return protobuf.decodeFromByteArray(Node.serializer(), value).value
         }
 
         @TypeConverter
         override fun fromList(list: List<Int>): ByteArray {
-            return protobuf.encodeToByteArray(ListSerializer(Int.serializer()), list)
+            return protobuf.encodeToByteArray(Node.serializer(), Node(list))
         }
     }
 
     object TagList : ProtoConverter<List<Tag>> {
+        @Serializable
+        private class Node(val value: List<Tag>)
+
         @TypeConverter
         override fun fromByteArray(value: ByteArray): List<Tag> {
-            return protobuf.decodeFromByteArray(ListSerializer(Tag.serializer()), value)
+            return protobuf.decodeFromByteArray(Node.serializer(), value).value
         }
 
         @TypeConverter
         override fun fromList(list: List<Tag>): ByteArray {
-            return protobuf.encodeToByteArray(ListSerializer(Tag.serializer()), list)
+            return protobuf.encodeToByteArray(Node.serializer(), Node(list))
         }
     }
 
     object IntArrayConverter : ProtoConverter<IntArray> {
+        @Serializable
+        private class Node(val value: IntArray)
+
         @TypeConverter
         override fun fromByteArray(value: ByteArray): IntArray {
-            return protobuf.decodeFromByteArray(IntArraySerializer(), value)
+            return protobuf.decodeFromByteArray(Node.serializer(), value).value
         }
 
         @TypeConverter
         override fun fromList(list: IntArray): ByteArray {
-            return protobuf.encodeToByteArray(IntArraySerializer(), list)
+            return protobuf.encodeToByteArray(Node.serializer(), Node(list))
         }
     }
 }
